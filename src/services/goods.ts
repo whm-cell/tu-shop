d3446d9df1c2c90467bd83301d58cf990f1b0f34
whm_ <fighting282367079@gmail.com>
2023-11-29 22:02:41 +0800

    商品页面的渲染

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,7 +1,8 @@
 import { http } from '@/utils/http'
+import type { GoodsResult } from '@/types/goods'
 
 export const getGoodsDetailApi = (id: string) => {
-  return http({
+  return http<GoodsResult>({
     method: 'GET',
     url: '/goods',
     data: {
