4607753529b3585464645fa1b4cd902009afe67a
whm_ <fighting282367079@gmail.com>
2023-12-03 20:26:41 +0800

    feat: 时间 国家 头像 日期控件的实现

---
+++ src/services/profile.ts
@@ -0,0 +1,12 @@
+import { http } from '@/utils/http'
+import type { ProfileDetail } from '@/types/member'
+
+/**
+ * 获取个人信息
+ */
+export const getMemberProfileApi = () => {
+  return http<ProfileDetail>({
+    method: 'GET',
+    url: '/member/profile',
+  })
+}
